78c619aa999bb77389a6cf612f97090d1f6e4025
Your NameBill <hoerrsolutions@gmail.com>
2022-05-27 21:21:59 -0400

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us | Sport Auto Group">
+      <p>We are Florida's #1 Automotive Dealer Group.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
