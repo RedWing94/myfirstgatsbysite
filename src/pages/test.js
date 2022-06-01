2d1c13911ee14427ec763ae8b0386fb4706fbb3c
James Vela <jamesjjimmy@yahoo.com>
2022-06-01 17:20:33 -0400

    added tailwind nav in react

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,149 +1,211 @@
-import * as React from 'react'
-/* This example requires Tailwind CSS v2.0+ */
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
 import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Menu, Popover, Transition } from '@headlessui/react'
+import { SearchIcon } from '@heroicons/react/solid'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
+const user = {
+  name: 'Chelsea Hagon',
+  email: 'chelsea.hagon@example.com',
+  imageUrl:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
 const navigation = [
-  { name: 'Shop', href: '#' },
-  { name: 'Sell/Trade', href: '#' },
-  { name: 'Finance', href: '#' },
-  { name: 'Service', href: '#' },
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Teams', href: '#', current: false },
+  { name: 'Directory', href: '#', current: false },
 ]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Example() {
   return (
-    <div className="relative bg-white overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-
-          <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                      <span className="sr-only">Workflow</span>
-                      <img
-                        alt="Workflow"
-                        className="h-8 w-auto sm:h-10"
-                        src="https://lh3.googleusercontent.com/ftHz7PJYfzNTWhmSM9mrJ0XIknExJNaOdNHGoyfV6-7gRxpOip1XMgu48NCBHINektCeaQQy2s7Y1gLdBmw-hw6VKiE3fYtRSzMf7ms4OWMStNK5stvIs3Mwe8kl1fV4ZfxMDqqn4E6uLr7rnI6W2KdgkLyEZET7VLdk3aFuf5_az5bvgweuzLIGys0whec-nZqgTk_sNxGUzwpc5VGmG6hy7XzZtgotZKcSX_8nyJcuXvW5d6UpssZVIHXJqP4uPm4V2li8-ZZY_vF0__Wwkkr3dSc3bo8PNk9HT1ecA__YQH9oz4sOoDMTvHmMrEFOXkRN-1YD8JFu3L72BAdLOL1-0_o9iTIKnz6NY1rG0G_fEiiGbum0MemLSc6blKvkUP3IxZAeWYRKGq13HKZq45BIg0gnCuU_xqWlyuMhVI-nLAtALI0uTo_yf2ZlBuWdgovcDDttbLfeVFE9FOo9-seAjMPGRJSEOJM3iDb9WmyI1pu_QKcr9G66v_Shtf77rwZo8aJzXOYHLC2lQbQT1_lPncTYwMQafy91ld7w8d3QuudHVhNgsMsn3YYIMoR2nNfTY3jeBed1bJMJAWgfrlRg3rw9_ZPF5BRX_BzdGoiTVRf2_2XBpmhvIkX5k6aFhaMtQXidaR7CDPTEN42Thq6lWIMUYxcYAabuWi_9PnSeZZr9ehkOwR2kUcdGXo1OmeI0qYdWvEwfHneoesgenqnvaNnPele-29RWtaOSaIGxXTPU9s4ekHqPhvL2JtVpjdFFp8SVlAT9CBTNjOOxh0aevxTsI9VgT4ugJ-EX0FWE1u0MzdfzK1BCp4j3Um_W8NS6=w1869-h975-no?authuser=0"
-                      />
-                    </a>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Open main menu</span>
-                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+      <>
+        {/* When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars */}
+        <Popover
+            as="header"
+            className={({ open }) =>
+                classNames(
+                    open ? 'fixed inset-0 z-40 overflow-y-auto' : '',
+                    'bg-white shadow-sm lg:static lg:overflow-y-visible'
+                )
+            }
+        >
+          {({ open }) => (
+              <>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <div className="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
+                    <div className="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
+                      <div className="flex-shrink-0 flex items-center">
+                        <a href="#">
+                          <img
+                              className="block h-8 w-auto"
+                              src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                              alt="Workflow"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+                      <div className="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
+                        <div className="w-full">
+                          <label htmlFor="search" className="sr-only">
+                            Search
+                          </label>
+                          <div className="relative">
+                            <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                              <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </div>
+                            <input
+                                id="search"
+                                name="search"
+                                className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                placeholder="Search"
+                                type="search"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden">
+                      {/* Mobile menu button */}
+                      <Popover.Button className="-mx-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Open menu</span>
+                        {open ? (
+                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        ) : (
+                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                        )}
                       </Popover.Button>
                     </div>
-                  </div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  {navigation.map((item) => (
-                    <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                      {item.name}
-                    </a>
-                  ))}
-                  <a href="#" className="font-medium text-cyan-600 hover:text-indigo-500">
-                    Log in
-                  </a>
-                </div>
-              </nav>
-            </div>
-
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://lh3.googleusercontent.com/ftHz7PJYfzNTWhmSM9mrJ0XIknExJNaOdNHGoyfV6-7gRxpOip1XMgu48NCBHINektCeaQQy2s7Y1gLdBmw-hw6VKiE3fYtRSzMf7ms4OWMStNK5stvIs3Mwe8kl1fV4ZfxMDqqn4E6uLr7rnI6W2KdgkLyEZET7VLdk3aFuf5_az5bvgweuzLIGys0whec-nZqgTk_sNxGUzwpc5VGmG6hy7XzZtgotZKcSX_8nyJcuXvW5d6UpssZVIHXJqP4uPm4V2li8-ZZY_vF0__Wwkkr3dSc3bo8PNk9HT1ecA__YQH9oz4sOoDMTvHmMrEFOXkRN-1YD8JFu3L72BAdLOL1-0_o9iTIKnz6NY1rG0G_fEiiGbum0MemLSc6blKvkUP3IxZAeWYRKGq13HKZq45BIg0gnCuU_xqWlyuMhVI-nLAtALI0uTo_yf2ZlBuWdgovcDDttbLfeVFE9FOo9-seAjMPGRJSEOJM3iDb9WmyI1pu_QKcr9G66v_Shtf77rwZo8aJzXOYHLC2lQbQT1_lPncTYwMQafy91ld7w8d3QuudHVhNgsMsn3YYIMoR2nNfTY3jeBed1bJMJAWgfrlRg3rw9_ZPF5BRX_BzdGoiTVRf2_2XBpmhvIkX5k6aFhaMtQXidaR7CDPTEN42Thq6lWIMUYxcYAabuWi_9PnSeZZr9ehkOwR2kUcdGXo1OmeI0qYdWvEwfHneoesgenqnvaNnPele-29RWtaOSaIGxXTPU9s4ekHqPhvL2JtVpjdFFp8SVlAT9CBTNjOOxh0aevxTsI9VgT4ugJ-EX0FWE1u0MzdfzK1BCp4j3Um_W8NS6=w1869-h975-no?authuser=0"
-                        alt=""
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
-                    {navigation.map((item) => (
+                    <div className="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
                       <a
-                        key={item.name}
-                        href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                          href="#"
+                          className="ml-5 flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
-                        {item.name}
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
                       </a>
+
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="flex-shrink-0 relative ml-5">
+                        <div>
+                          <Menu.Button className="bg-white rounded-full flex focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span className="sr-only">Open user menu</span>
+                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none">
+                            {userNavigation.map((item) => (
+                                <Menu.Item key={item.name}>
+                                  {({ active }) => (
+                                      <a
+                                          href={item.href}
+                                          className={classNames(
+                                              active ? 'bg-gray-100' : '',
+                                              'block py-2 px-4 text-sm text-gray-700'
+                                          )}
+                                      >
+                                        {item.name}
+                                      </a>
+                                  )}
+                                </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+
+                      <a
+                          href="#"
+                          className="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        New Project
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
+                  <div className="max-w-3xl mx-auto px-2 pt-2 pb-3 space-y-1 sm:px-4">
+                    {navigation.map((item) => (
+                        <a
+                            key={item.name}
+                            href={item.href}
+                            aria-current={item.current ? 'page' : undefined}
+                            className={classNames(
+                                item.current ? 'bg-gray-100 text-gray-900' : 'hover:bg-gray-50',
+                                'block rounded-md py-2 px-3 text-base font-medium'
+                            )}
+                        >
+                          {item.name}
+                        </a>
                     ))}
                   </div>
-                  <a
-                    href="#"
-                    className="block w-full px-5 py-3 text-center font-medium text-cyan-600 bg-gray-50 hover:bg-gray-100"
-                  >
-                    Log in
-                  </a>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block text-cyan-600 xl:inline">Sport Automotive</span>{' '}
-                <span className="block text-black xl:inline">Dealer Group</span>
-              </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Sport Automotive Group is Florida's #1 automotive dealer group. Online. In-Store. At Your Service. We have been serving the Greater Orlando community for over 
-                50 years. 
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-800 md:py-4 md:text-lg md:px-10"
-                  >
-                    Shop Cars
-                  </a>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-cyan-600 bg-cyan-100 hover:bg-cyan-200 md:py-4 md:text-lg md:px-10"
-                  >
-                    Sell/Trade
-                  </a>
-                </div>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-      
-    </div>
+                  <div className="border-t border-gray-200 pt-4 pb-3">
+                    <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
+                      <div className="flex-shrink-0">
+                        <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium text-gray-800">{user.name}</div>
+                        <div className="text-sm font-medium text-gray-500">{user.email}</div>
+                      </div>
+                      <button
+                          type="button"
+                          className="ml-auto flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                    <div className="mt-3 max-w-3xl mx-auto px-2 space-y-1 sm:px-4">
+                      {userNavigation.map((item) => (
+                          <a
+                              key={item.name}
+                              href={item.href}
+                              className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                          >
+                            {item.name}
+                          </a>
+                      ))}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </>
+          )}
+        </Popover>
+      </>
   )
 }
