4072afb31ab5a11cbecb8aa2cd0834c8b84c35ea
RedWing94 <hoerrsolutions@gmail.com>
2022-06-03 16:39:30 -0400

    Removed Bell Icon
    Removed Bell Icon

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -107,11 +107,8 @@ export default function Example() {
                           href="#"
                           className="ml-5 flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                        
                       </a>
-
-                      
 
                       <a
                           href="#"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Sport Automotive Group">
-      <StaticImage
-        alt="Ford F-150"
-        src="../images/ImagePlaceholderHero-01.jpg"
-      />
+    <Layout>
     </Layout>
   )
 }
